e888144be1b98417f9086745ccbeef184502b509
Samuel Filizzola <samuelfilizzola@Samuels-Mac-mini.local>
2014-10-01 10:31:20 -0300

    Initial Commit

---
+++ Mobile Track/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mobile Track
+//
+//  Created by Samuel Filizzola on 01/10/14.
+//  Copyright (c) 2014 ECX Card. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
